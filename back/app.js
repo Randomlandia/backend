1eabd06b886ea7a1c6ed97f3ef6b887b55dd6414
aluzdev <aluzdev@gmail.com>
2024-05-30 00:16:19 -0600

    feat: user CRUD operations + Password Encryption (#1)
    * feat: create user
    
    * fix: module not found
    
    * get users
    
    * feat: user achievements
    
    * feat: user validates existing sandias
    
    * feat: delete user by id
    
    * feat: getbyID and update user
    
    * less worse code

--- back/app.js
+++ back/app.js
@@ -1,27 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT
-const db = require('./src/db/mongo')
-const sandia = require('./src/routes/sandia')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT;
+const db = require("./src/db/mongo");
+const sandia = require("./src/routes/sandia");
+const users = require("./src/routes/users");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/wm", sandia)
+app.use("/wm", sandia);
+app.use("/users", users);
 
 app.get("/", (req, res) => {
-  res.status(200).send({ message: 'Randomizing your experience from here!' })
-})
+  res.status(200).send({ message: "Randomizing your experience from here!" });
+});
 
 db.connect
   .then((message) => {
-    console.log(message)
+    console.log(message);
     app.listen(port, () => {
-      console.log(`Server listening on ${port} port.`)
-    })
+      console.log(`Server listening on ${port} port.`);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
