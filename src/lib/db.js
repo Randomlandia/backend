cdc7eaaf798ca1fdef54d5bc89f0dfcec6f4a8b4
aluz <aluzdev@gmail.com>
2024-06-12 13:29:07 -0500

    refactor: database connection; server definition

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,14 @@
+//todo lo que no está directamente relacionado con los casos de uso, rutas o modelos
+//y se puede compartir, va en lib
 const mongoose = require("mongoose");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
-const connect = new Promise(async (resolve, reject) => {
-  const conn = mongoose.connect(url);
-  if (conn) resolve("Connection succesfully.");
-  reject(new Error("Error connection failed"));
-});
+function connect() {
+  return mongoose.connect(url);
+}
 
 module.exports = {
   connect,
